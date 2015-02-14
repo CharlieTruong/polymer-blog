3518fdece1ffa91e885146a527b777ae81d0e074
Charlie Truong <charlie.m.truong@gmail.com>
2015-02-14 16:39:23 -0600

    Using vulcanize to build the project. Rebuilding the project on each html file change.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,37 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
- 
+var vulcanize = require('gulp-vulcanize');
+
+var build = function() {
+  var DEST_DIR = 'dist';
+  return gulp.src('index.html')
+    .pipe(vulcanize({
+      dest: DEST_DIR,
+      strip: true,
+      csp: true,
+      inline: true
+    }))
+    .pipe(gulp.dest(DEST_DIR));
+};
+
 gulp.task('connect', function() {
   connect.server({
+    root: 'dist',
     livereload: true
   });
 });
 
-gulp.task('html', function () {
-  gulp.src('**/*.html')
-    .pipe(connect.reload());
+gulp.task('reload', function () {
+  build()
+  .pipe(connect.reload());
 });
  
 gulp.task('watch', function () {
-  gulp.watch(['**/*.html'], ['html']);
+  gulp.watch(['index.html', 'elements/**/*.html'], ['reload']);
 });
- 
-gulp.task('default', ['connect', 'watch']);
+
+gulp.task('build', function() {
+  build();
+});
+
+gulp.task('default', ['build', 'connect', 'watch']);
